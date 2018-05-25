e9cb9a39955e4ab3e558765cc64873a4f9e0ab8c
18743206721 <18743206721>
2018-05-25 09:36:56 +0800

    1

---
+++ app/src/main/java/com/xingguang/master/App.java
@@ -0,0 +1,9 @@
+package com.xingguang.master;
+
+/**
+ * 创建日期：2018/5/25
+ * 描述:
+ * 作者:LiuYu
+ */
+public class App {
+}
---
+++ app/src/main/java/com/xingguang/master/MainActivity.java
@@ -0,0 +1,19 @@
+package com.xingguang.master;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * 创建日期：2018/5/25
+ * 描述:主页
+ * 作者:LiuYu
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
